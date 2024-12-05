2b7bdf8b99c426ca3c8f929102f321f214f51c2f
TeamB4-GP <TeamB4@GaryPedretti.com>
2024-12-05 17:47:50 +0000

    add unit test

--- GameController_Tests/checkIsHitTests.js
+++ GameController_Tests/checkIsHitTests.js
@@ -1,83 +1,27 @@
 const assert = require('assert').strict;
+const cliColor = require('cli-color');
+const { CheckFleetSunkenShips } = require("../battleship.js");
 const gameController = require("../GameController/gameController.js");
 const letters = require("../GameController/letters.js");
 const position = require("../GameController/position.js");
 
-describe('checkIsHitTests', function () {
+describe('CheckFleetSunkenShipsTests', function () {
 
-  it('should return true for isShotOnTarget if there is a ship at the shooting position', function () {
+  it('should return one sunken ship when one ship in the fleet is sunk', function () {
     var ships = gameController.InitializeShips();
-    counter = 1;
-    ships.forEach(ship => {
-      for (var i = 1; i <= ship.size; i++) {
-        column = letters.get(counter);
-        ship.addPosition(new position(letters.get(counter), i));
-      }
-      counter++;
-    });
-    var result = gameController.CheckIsHit(ships, new position(letters.A, 1));
-    assert.strictEqual(result.isShotOnTarget, true, "shot was not on target");
-    assert.strictEqual(result.isNewHit, true, "shot was not a new hit");
-  });
 
-  it('should return false for isShotOnTarget if there is no ship at the shooting position', function () {
-    var ships = gameController.InitializeShips();
-    counter = 1;
-    ships.forEach(ship => {
-      for (var i = 1; i <= ship.size; i++) {
-        ship.addPosition(new position(letters.get(counter), i));
-      }
-      counter++;
-    });
-    var result = gameController.CheckIsHit(ships, new position(letters.G, 1));
-    assert.strictEqual(result.isShotOnTarget, false);
-    assert.strictEqual(result.isNewHit, false);
-  });
+    // Set up a single ship and mark it as sunk
+    ships[0].addPosition(new position(letters.A, 1));
+    ships[0].addPosition(new position(letters.A, 2));
+    ships[0].addPosition(new position(letters.A, 3));
 
-  it('should throw an exception if position is undefined', function () {
-    var ships = gameController.InitializeShips();
-    assert.throws(
-      () => {
-        gameController.CheckIsHit(ships, undefined);
-      },
-      /The shooting position is not defined/ // Check for the specific error message
-    );
-  });
+    ships[0].checkHit(new position(letters.A, 1))
+    ships[0].checkHit(new position(letters.A, 2));
+    ships[0].checkHit(new position(letters.A, 3));
 
-  it('should throw an exception if ships is undefined', function () {
-    assert.throws(
-      () => {
-        gameController.CheckIsHit(undefined, new position(letters.G, 1));
-      },
-      /No ships defined/ // Check for the specific error message
-    );
-  });
+    const result = CheckFleetSunkenShips(ships);
 
-  it('should return false for isNewHit if the position was already hit', function () {
-    var ships = gameController.InitializeShips();
-    counter = 1;
-    ships.forEach(ship => {
-      for (var i = 1; i <= ship.size; i++) {
-        column = letters.get(counter);
-        ship.addPosition(new position(letters.get(counter), i));
-      }
-      counter++;
-    });
-
-    // First hit
-    var firstResult = gameController.CheckIsHit(ships, new position(letters.A, 1));
-    assert.strictEqual(firstResult.isShotOnTarget, true, "First hit was not on target");
-    assert.strictEqual(firstResult.isNewHit, true, "First hit was not a new hit");
-
-    // Second hit at the same position
-    var secondResult = gameController.CheckIsHit(ships, new position(letters.A, 1));
-    assert.strictEqual(secondResult.isShotOnTarget, true, "Second hit was not on target");
-    assert.strictEqual(secondResult.isNewHit, false, "Second hit was incorrectly marked as new");
-
-    // Third hit at the same position
-    var thirdResult = gameController.CheckIsHit(ships, new position(letters.A, 1));
-    assert.strictEqual(thirdResult.isShotOnTarget, true, "Third hit was not on target");
-    assert.strictEqual(thirdResult.isNewHit, false, "Third hit was incorrectly marked as new");
+    assert.deepStrictEqual(result, [ships[0].name], "The function did not correctly identify the sunk ship");
   });
 
 });
